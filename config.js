69f706d89df2ff95e3ba952d91eb1ecbdc171961
Bichu X Boy <bichuxboy@gmail.com>
2026-08-21 00:37:02 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: '8888400515:AAGXEqspx8e6bpWx7rV4BgT-a3gexKCN6K8', // Bot Token Here 
+    
+    S7: 'Bichu Mini BUG', // Owner, Name
+    
+    adminId: '8763895360', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/bichuxboy1', // TG channel link
+    
+    group: 'https://t.me/+pMIeOYHuLEQ3YmRk', // TG group link
+    
+    bot: 'Bichu Mini BUG', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
